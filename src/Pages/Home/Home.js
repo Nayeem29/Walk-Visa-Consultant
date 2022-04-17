cfbdcfc74c1db46eddf56f59bb4dc925b221e64f
Nayeem29 <nayeembracu29@gmail.com>
2022-04-17 16:30:28 +0600

    heading and cover photo add

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import coverImg from '../../Assets/coverphoto.png';
 
 const Home = () => {
   return (
     <div>
-
+      <section className='md:flex justify-between items-center bg-gray-400'>
+        <div className='mx-auto text-center'>
+          <h1 className='text-6xl font-bold text-white'>Get the Best Colsultancy <br />
+            Secure your future with us</h1>
+          <p className='mx-36 text-base font-medium my-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, suscipit quia voluptatum dignissimos assumenda animi reiciendis ullam quae molestias iure.</p>
+          <button className='mt-16 border-2 px-20 py-2 rounded-2xl bg-purple-400 font-semibold'>Learn More</button>
+        </div>
+        <img className='w-1/3 h-1/3 mx-auto rounded-sm' src={coverImg} alt="" />
+      </section>
     </div>
   );
 };
--- src/Pages/SharedPages/Header/Header.js
+++ src/Pages/SharedPages/Header/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>
-
+    <div className='flex justify-between items-center h-12 bg-purple-400'>
+      <p className='text-2xl font-semibold pl-10'>Walk Visa Consultant</p>
+      <div className='mx-10'>
+        <NavLink className='px-5' to='/'>Home</NavLink>
+        <NavLink className='px-5' to='/services'>Services</NavLink>
+        <NavLink className='px-5' to='/about'>About</NavLink>
+        <NavLink className='px-5' to='/login'>Login</NavLink>
+        <NavLink className='px-5' to='/sign up'>Sign up</NavLink>
+      </div>
     </div>
   );
 };
